e59816e4d23d03305980e3eed0048ab284847046
roberto-vera <robertodevstack@gmail.com>
2023-05-23 16:16:31 -0400

    commit

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -6,11 +6,16 @@ const Login = () => {
     const [usuario, setUsuario] = useState('');
     const [contrasena, setContrasena] = useState('');
     const [mensaje, setMensaje] = useState('');
+    const [botonDesactivado, setBotonDesactivado] = useState(true);
+
     const handleNombreChange = (e) => {
         setUsuario(e.target.value);
+        setBotonDesactivado(e.target.value === '' || contrasena === '');
+
     }
     const handleContrasenaChange = (e) => {
         setContrasena(e.target.value)
+        setBotonDesactivado(usuario === '' || e.target.value === '');
     }
     const handleSubmit = (event) => {
         event.preventDefault();
@@ -24,7 +29,7 @@ const Login = () => {
             <div className='form-group text-center w-25'>
                 <input className='container form-control' value={usuario} name='usuario' type='text' onChange={handleNombreChange} />
                 <input className='container form-control' value={contrasena} type='password' onChange={handleContrasenaChange}/>
-                <button type='submit text-center' className='btn btn-primary'>Enviar</button>
+                <button type='submit text-center' className='btn btn-primary' disabled={botonDesactivado}>Enviar</button>
             </div>
         </form>
         <MensajeValidacion className="mensaje" mensaje={mensaje} />
